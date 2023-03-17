648479497bd1f3d3a474cafa811adc6ccc2d47e6
smile0711 <98184756+smile0711@users.noreply.github.com>
2023-03-17 17:23:22 -0500

    Update NeighborhoodPage.vue

--- spa/src/pages/neighborhood/NeighborhoodPage.vue
+++ spa/src/pages/neighborhood/NeighborhoodPage.vue
@@ -64,7 +64,7 @@ export default Vue.extend({
         this.hood = response[0].data.hood;
         this.colony = response[0].data.colony;
         this.blocks = response[1].data.blocks;
-        this.$store.methods.setPlace(response[0].data);
+        this.$store.methods.setPlace(this.hood);
         document.title = this.hood.name + " - Cybertown";
       });
     },
